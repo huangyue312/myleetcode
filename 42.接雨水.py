b5b71ee3afd2e26796d0c092973f669cc45829b6
huangyue312 <huangyue312@163.com>
2020-05-29 16:05:05 +0800

    update 71\42

---
+++ 42.接雨水.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=42 lang=python3
+#
+# [42] 接雨水
+#
+# https://leetcode-cn.com/problems/trapping-rain-water/description/
+#
+# algorithms
+# Hard (51.02%)
+# Likes:    1305
+# Dislikes: 0
+# Total Accepted:    103.7K
+# Total Submissions: 203.1K
+# Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+#
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+# 
+# 
+# 
+# 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢
+# Marcos 贡献此图。
+# 
+# 示例:
+# 
+# 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+# 输出: 6
+# 
+#
+
+# @lc code=start
+class Solution:
+    def trap(self, height) -> int:
+        stack=[height[0]]
+        for i in range(1,len(height)):
+            sum_1=0
+            l=[]
+            if height[i]>=stack[-1] and 
+               l.append[stack.pop()]
+               l.append[height[i]]
+               sum_1=sum_1+len(l)*min(l)-sum(l)
+
+
+            if i > stack.pop()
+
+        
+        
+
+# @lc code=end
+
