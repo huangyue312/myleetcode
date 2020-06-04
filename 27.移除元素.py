e83402bdcbdb0712196c9424b2378c0f577ea046
huangyue312 <huangyue312@163.com>
2020-06-04 15:38:48 +0800

    update 27/28

--- 27.移除元素.py
+++ 27.移除元素.py
@@ -66,6 +66,12 @@
 # @lc code=start
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        l=0
+        for i in range(0,len(nums)):
+            if nums[i]!=val:
+                nums[l]=nums[i]
+                l+=1
+        return l
+
 # @lc code=end
 
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-nums=[]
-s=1
-for i in range(0,len(nums)-1):
-    if nums[i]!=nums[i+1]:
-        nums[s]=nums[i+1]
-        s+=1
-print s
+a="hello"
+print(a[1:4])
