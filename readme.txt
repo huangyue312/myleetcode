3e1a1bdb823f2619c7ec0bb4bb8a26b12e165fee
huangyue312 <huangyue312@163.com>
2020-06-10 16:27:18 +0800

    update 88

--- readme.txt
+++ readme.txt
@@ -4,3 +4,4 @@ hard 42  接雨水
 Medium 2 两数之和
 simple 53 最大子序和
 simple 34 在排序数组中查找元素的第一个和最后一个位置
+simple 88 合并两个有序数组
--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-s=" "
-a=s.split(' ')
-print(a)
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+     """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    
+    if m==0:  
+        for k in range(n):
+            nums1[k]=nums2[k]
+    else:
+        i=j=0
+        while j<len(nums2):
+            if nums1[m+j-1]<=nums2[j]:
+                nums1[m+j]=nums2[j]
+                j+=1
+                    
+            else:
+                for i in range(m+n):
+                    if nums1[i]>nums2[j]:
+                        t=nums1[i]
+                        nums1[i]=nums2[j]
+                        nums2[j]=t
