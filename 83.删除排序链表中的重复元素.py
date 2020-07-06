c3eb99ae6c3e08e1dca9f56174deb938d5d87556
huangyue312 <huangyue312@163.com>
2020-07-06 16:15:54 +0800

    update 83

---
+++ 83.删除排序链表中的重复元素.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode.cn id=83 lang=python3
+#
+# [83] 删除排序链表中的重复元素
+#
+# https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+#
+# algorithms
+# Easy (50.73%)
+# Likes:    340
+# Dislikes: 0
+# Total Accepted:    114.3K
+# Total Submissions: 225.3K
+# Testcase Example:  '[1,1,2]'
+#
+# 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+# 
+# 示例 1:
+# 
+# 输入: 1->1->2
+# 输出: 1->2
+# 
+# 
+# 示例 2:
+# 
+# 输入: 1->1->2->3->3
+# 输出: 1->2->3
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        s=head
+        #s_v=s.val
+        while s.next :
+            e=s.next
+            if s.val==e.val:
+                e=e.next
+            else:
+                s=e
+        return head
+
+# @lc code=end
+
