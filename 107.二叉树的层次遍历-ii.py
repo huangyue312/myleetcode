6cd2223e48e85e4d32b48ac33f67d6c9896b988d
huangyue312 <huangyue312@163.com>
2020-07-12 14:54:03 +0800

    update

--- 107.二叉树的层次遍历-ii.py
+++ 107.二叉树的层次遍历-ii.py
@@ -20,7 +20,7 @@ class Solution:
     def bfs (self,c,treenode,res):
         if treenode:
             #c=c+1 
-            if len(res)<c+1:
+            if len(res)<c+1: 
                 res.insert(0,[])
             res[-(c+1)].append(treenode.val)
             self.bfs(c+1,treenode.left,res)
