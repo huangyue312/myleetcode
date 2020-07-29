b30f9c42a2c62b78c424e174f7132649dac2a41a
huangyue312 <huangyue312@163.com>
2020-07-29 16:53:23 +0800

    update 136

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-flag=0
-a=list("11")
-b=list("1")
-re=""
-while flag or len(a)>0 or len(b)>0:
-    nums_1= int(a.pop()) if len(a)>0 else 0
-    nums_2= int(b.pop()) if len(b)>0 else 0
-    sum_item =flag + nums_1 + nums_2
-    item=sum_item % 2
-    re=re+str(item)
-    flag=sum_item//2
-print(re[::-1])
+a=0
+b=2
+c=a^b
+c=int(c)
+print(c)
